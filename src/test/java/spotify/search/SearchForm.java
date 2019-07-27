bf7a1be89a1db4d3722fbb32b2b1c88a3a4b0f53
Julian <jdmesalosada@gmail.com>
2019-07-27 10:02:29 -0500

    first commit

---
+++ src/test/java/spotify/search/SearchForm.java
@@ -0,0 +1,8 @@
+package spotify.search;
+
+import org.openqa.selenium.By;
+
+class SearchForm {
+    static By SEARCH_FIELD = By.cssSelector(".js-search-input");
+    static By SEARCH_BUTTON = By.cssSelector(".js-search-button");
+}
